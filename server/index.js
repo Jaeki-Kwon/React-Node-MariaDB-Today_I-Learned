979ba3d7b9d1522a63e918719a6fa20d8e60881f
Jaeki-Kwon <worl1126@naver.com>
2021-08-29 15:45:37 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join("../client/build")));
 
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  res.sendFile(path.resolve("../client", "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 8000;
